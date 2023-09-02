766aa2f16b54f034a29b80d24cb0d79763d17718
fred <fredjuma8@gmail.com>
2023-09-02 13:29:54 +0300

    lights camera action

--- client/src/canvas/CameraRig.jsx
+++ client/src/canvas/CameraRig.jsx
@@ -13,29 +13,30 @@ const CameraRig = ({ children }) => {
     const isBreakpoint = window.innerWidth <= 1260;
     const isMobile = window.innerWidth <= 600;
 
-    // initial camera position
+    // set the initial position of the model
     let targetPosition = [-0.4, 0, 2];
     if(snap.intro) {
-      if(isBreakpoint) targetPosition = [ 0, 0, 2];
-      if(isMobile) targetPosition = [ 0, 0.2, 2.5];
+      if(isBreakpoint) targetPosition = [0, 0, 2];
+      if(isMobile) targetPosition = [0, 0.2, 2.5];
     } else {
-      if(isMobile) targetPosition = [ 0, 0.2, 2.5];
-      else targetPosition = [ 0, 0.2, 2.5];
+      if(isMobile) targetPosition = [0, 0, 2.5]
+      else targetPosition = [0, 0, 2];
     }
 
-    // model camera position
+    // set model camera position
     easing.damp3(state.camera.position, targetPosition, 0.25, delta)
 
-  // model rotation
-  easing.dampE(
-    group.current.rotation,
-    [state.pointer.y / 10, -state.pointer.x / 5, 0],
-    0.25,
-  )
-})
+    // set the model rotation smoothly
+    easing.dampE(
+      group.current.rotation,
+      [state.pointer.y / 10, -state.pointer.x / 5, 0],
+      0.25,
+      delta
+    )
+  })
 
 
-return <group ref={group}>{children}</group>
+  return <group ref={group}>{children}</group>
 }
 
 export default CameraRig

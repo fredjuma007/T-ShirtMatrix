b9a68a725d821c5d40ae6c59fe15bf79d0434fdc
fred <fredjuma8@gmail.com>
2023-09-02 12:24:36 +0300

    fixed the rendering

--- client/src/canvas/index.jsx
+++ client/src/canvas/index.jsx
@@ -11,13 +11,13 @@ const CanvasModel = () => {
       <ambientLight intensity={0.5} />
       <Environment preset="city" />
 
-      {/*<CameraRig>*/}
+      <CameraRig>
         {/*<Backdrop />*/}
         <Center>
           <Shirt />
         </Center>
 
-     {/* </CameraRig>*/}
+     </CameraRig>
     </Canvas>
   )
 }
--- client/src/store/index.js
+++ client/src/store/index.js
@@ -4,8 +4,8 @@ const state = proxy({
 //whatever you want to store to use in the entire app
 intro: true,
 color: '#EFBD4B',
-isLogoText: true,
-isFullText: false,
+isLogoTexture: true,
+isFullTexture: false,
 logoDecal: './threejs.png',
 fullDecal: './threejs.png',
 
